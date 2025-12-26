e1d78922268fabb3a4fd9aa815939a9eb696e800
Arraya <markillerth0000@gmail.com>
2025-12-26 20:53:32 +0700

    Done

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,38 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    double sum = 0.0;
+    double sum_of_square = 0.0;
+    string textline;
+
+    ifstream source("score.txt");
+
+    while (getline(source, textline))
+    {
+        double x = stod(textline);
+        sum += x;
+        sum_of_square += x * x;
+        count++;
+    }
+
+    double mean = sum / count;
+    double sd = sqrt((sum_of_square / count) - (mean * mean));
+
+    cout << "Number of data = " << count << "\n";
+
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+
+    cout << fixed << setprecision(3);
+    cout << "Standard deviation = " << sd;
+
+    return 0;
+}
